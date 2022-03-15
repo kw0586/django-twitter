667dce119ec588f2000906dd0bf1ef04048a4c93
JHQ2015 <88753692+JHQ2015@users.noreply.github.com>
2022-03-15 19:13:31 -0400

    Login, Login Status, Logout, Signup API

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,48 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from rest_framework import exceptions
 
-
+# serializer的用处： 1. 在前段渲染用户的object的时候，渲染成json格式。 2. 用来验证用户是否存在
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email')
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+class SignupSerializer(serializers.ModelSerializer): #用modelserializer的话，用serializer.save的时候，会把用户创建出来，所以会新增一个class meta
+    username = serializers.CharField(max_length = 20,min_length = 6)
+    password = serializers.CharField(max_length = 20,min_length = 6)
+    email = serializers.EmailField() #会自动有email的检测机制
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    # will be called wehn is_valid is called
+    def validate(self, data):
+        # TODO<HOMEWORK> 增加验证 username 是不是只由给定的字符集合构成
+        if User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message': 'This username has been occupied.'
+            })
+        if User.objects.filter(email=data['email'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message': 'This email address has been occupied.'
+            })
+        return data
+
+    def create(self, validated_data):
+        username = validated_data['username'].lower()#存账号的时候，都换成小写
+        email = validated_data['email'].lower()
+        password = validated_data['password']
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+        return user
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,8 +1,20 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
+from rest_framework.response import Response #返回的都是response格式
 from rest_framework import permissions
-from accounts.api.serializers import UserSerializer
+from rest_framework.decorators import action
+from accounts.api.serializers import (
+    UserSerializer,
+    LoginSerializer,
+    SignupSerializer,
+)
 
+from django.contrib.auth import (
+    login as django_login,
+    logout as django_logout,
+    authenticate as django_authenticate,
+
+)
 #提供一个user的api，能够读取user的具体内容
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -11,3 +23,78 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined') #从哪去获取数据
     serializer_class = UserSerializer #数据怎么变成json
     permission_classes = [permissions.IsAuthenticated] #
+
+class AccountViewSet(viewsets.ViewSet): #不用内置的accountviewset，自己定义，所以继承一个空的viewset
+    serializer_class = SignupSerializer
+    @action(methods = ['GET'], detail = False)
+    # 指定是get的request
+    # viewset里自带的操作是增删查改，这边我们自己定义一个action，叫login_status
+        #定义在某个object上的动作，detail=False要取掉。如果是定义在整个根目录上的动作，detail = False 例如api/accounts/login_status (定义在根目录accounts上的动作);
+        #api/accouns/123/login_status 是定义在object123上的动作
+    def login_status(self,request):
+        data = {'has_logged_in': request.user.is_authenticated}
+        if request.user.is_authenticated:
+            data['user'] = UserSerializer(request.user).data
+        return Response(data)
+
+    @action(methods = ['POST'],detail =False)
+    def logout(self,request):
+        django_logout(request)
+        return Response(({'success':True}))
+
+    @action(methods = ['POST'], detail = False)
+    def login(self,request):
+        # get user name and password from request
+        serializer = LoginSerializer(data=request.data) #通过loginserializer 获取用户输入的数据
+        if not serializer.is_valid():
+            return Response({
+                "success": False,
+                "message": "Please check input",
+                "errors": serializer.errors,
+            }, status=400)
+        # validation ok, login
+        username = serializer.validated_data['username']
+        password = serializer.validated_data['password']
+
+        #如果user不存在的话
+        '''
+        下面这段queryset的代码常用于debug，看django里面的语句对应的sql语句是什么
+        '''
+        # queryset= User.objects.filter(username=username)
+        # print(queryset.query)
+
+        if not User.objects.filter(username=username).exists():
+            return Response({
+                "success": False,
+                "message": "username and password does not match",
+            }, status=400)
+
+        user = django_authenticate(username=username, password=password) # authenticate后的user才是能够使用的user
+        if not user or user.is_anonymous:
+            return Response({
+                "success": False,
+                "message": "username and password does not match",
+            }, status=400)
+
+        django_login(request, user)
+        return Response({
+            "success": True,
+            "user": UserSerializer(instance=user).data,
+        })
+
+    @action(methods = ['POST'], detail = False)
+    def signup(self,request):
+        serializer = SignupSerializer(data = request.data)
+        if not serializer.is_valid():
+            return Response({
+                "success": False,
+                "message": "Please check input",
+                "errors": serializer.errors,
+            }, status=400)
+
+        user = serializer.save()
+        django_login(request, user)
+        return Response({
+            'success': True,
+            'user': UserSerializer(user).data,
+        },status  =201)
